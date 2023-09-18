a9a9e23bd87ed37b979bb3c988a87df0e2e8c229
IceKing0109 <iceking0109@gmail.com>
2023-09-18 11:43:42 +0900

    [2023-09-18][Porting from React.js to Plain HTML/CSS/JavaScript]

--- src/helpers/shuffleArray.js
+++
@@ -1,7 +0,0 @@
-export default function shuffleArray(array) {
-    return array
-      .map(a => ({ sort: Math.random(), value: a }))
-      .sort((a, b) => a.sort - b.sort)
-      .map(a => a.value);
-  }
-  
